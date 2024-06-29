2d5aa1ad8f402a095d4f4b1b1a287bccbe850994
romeltined <rstined@yahoo.com>
2024-06-29 09:06:49 +0800

    enhancement

--- src/main/java/com/hello/earth/EarthApplication.java
+++ src/main/java/com/hello/earth/EarthApplication.java
@@ -11,7 +11,7 @@ public class EarthApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello Docker World in ubuntu";
 	}
 
 	public static void main(String[] args) {
