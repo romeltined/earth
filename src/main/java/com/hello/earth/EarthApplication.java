376e527b04f56c12cc0ea7731a22ab032f90ea52
romeltined <rstined@yahoo.com>
2024-07-12 23:32:48 +0800

    add json apple endpoint:

--- src/main/java/com/hello/earth/EarthApplication.java
+++ src/main/java/com/hello/earth/EarthApplication.java
@@ -2,8 +2,14 @@ package com.hello.earth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -12,6 +18,15 @@ public class EarthApplication {
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World in ubuntu";
+	}
+
+	//@GetMapping(path = "/apple")
+	@RequestMapping("/apple")
+	public ResponseEntity<Object> getSomeParameters() {
+		return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+				"key-1", "value-1",
+				"key-2", "value-2",
+				"key-3", "value-3"));
 	}
 
 	public static void main(String[] args) {
